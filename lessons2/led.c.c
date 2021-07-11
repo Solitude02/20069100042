414db0831cbb109c15ca651acfe70085464821dd
Solitude02 <87232560+Solitude02@users.noreply.github.com>
2021-07-11 22:41:00 +0800

    Add files via upload
    跑马灯

---
+++ lessons2/led.c.c
@@ -0,0 +1,22 @@
+// C++ code
+//
+void setup()
+{
+    pinMode(13, OUTPUT);
+	pinMode(12, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(10,OUTPUT);
+    pinMode(9,OUTPUT);
+    pinMode(8,OUTPUT);
+    pinMode(7,OUTPUT);
+}
+
+void loop()
+{
+  for(int i=13;i>6;i--)
+  {
+  digitalWrite(i, HIGH);
+  delay(200);
+  digitalWrite(i, LOW);
+  }
+}
